2905f1341cb954148b4599505329095d964aef1c
Patiphan Phengpao <misterpatipan123@gmail.com>
2024-07-19 19:18:21 +0700

    Update script.js

--- js-project-finished/assets/js/script.js
+++ js-project-finished/assets/js/script.js
@@ -1,10 +1,10 @@
-//---------- Toggle Hamburger Icon script ----------//
+// JavaScript Here
 
 function toggleHamburgerIcon(el) {
     el.classList.toggle("change");
 }
 
-//---------- Toggle mobile menu when hamburgerIcon is clicked script ----------//
+//---------- Toggle mobile menu when hamburger icon is clicked ----------//
 
 let hamburgerIcon = document.getElementById("hamburgerIcon");
 
@@ -16,11 +16,11 @@ if (hamburgerIcon) {
                 mobileMenu.style.display = "none";
             } else {
                 mobileMenu.style.display = "block";
-            }
+            } 
         } else {
             console.error("Element with ID 'mobileMenu' not found.");
         }
-    });
+    })
 } else {
     console.error("Element with ID 'hamburgerIcon' not found.");
 }
@@ -36,47 +36,43 @@ function plusSlides(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("banner-wrapper");
+    console.log(slides.length);
 
     if (n > slides.length) {
         slideIndex = 1;
-    }    
+    }
 
     if (n < 1) {
         slideIndex = slides.length;
     }
 
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+        slides[i].style.display = "none";
     }
 
-    slides[slideIndex-1].style.display = "flex";  
+    slides[slideIndex-1].style.display = "flex";
+
 }
+
 showSlides(slideIndex);
 
 //---------- Cars Img modal script ----------//
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+let modal = document.getElementById("myModal");
+let modalImg = document.getElementById("img01");
+let img = document.getElementsByClassName("myImg");
 
 for (let i = 0; i < img.length; i++) {
     img[i].addEventListener("click", function() {
         modal.style.display = "block";
         modalImg.src = this.src;
-    })
+    });
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+let closeBtn = document.getElementsByClassName("close")[0];
+closeBtn.addEventListener("click", function() {
+    modal.style.display = "none";
+});
 
 //---------- Cars Grid Info script ----------//
 
@@ -101,37 +97,34 @@ for (let i = 0; i < carCloseDescBtn.length; i++) {
 let acc = document.getElementsByClassName("accordion-btn");
 
 for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
 }
 
 //---------- Back to top script ----------//
 
-// Get the button:
-let mybutton = document.getElementById("myBtt");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-    scrollFunction()
-};
+let myBtt = document.getElementById("myBtt");
 
 function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        myBtt.style.display = "block";
+    } else {
+        myBtt.style.display = "none";
+    }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function backToTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function() {
+    scrollFunction();
 }
